19454c513a92135c558f80388b5e1db5414e8975
Tarasova Uliana <tarasova.ulya@gmail.com>
2024-12-30 12:22:17 +0300

    all tasks resolved

--- homework/tasks/task_3.py
+++ homework/tasks/task_3.py
@@ -10,15 +10,6 @@ class Ticket:
 
 
 async def coroutines_execution_order(coros: list[Awaitable[Ticket]]) -> str:
-    # Необходимо выполнить все полученные корутины, затем упорядочить их результаты
-    # по полю number и вернуть строку, состоящую из склеенных полей key.
-    #
-    # Пример:
-    # r1 = Ticket(number=2, key='мыла')
-    # r2 = Ticket(number=1, key='мама')
-    # r3 = Ticket(number=3, key='раму')
-    #
-    # Результат: 'мамамылараму'
-    #
-    # YOUR CODE GOES HERE
-
+    res = await asyncio.gather(*coros)
+    res.sort(key=lambda x: x.number)
+    return ''.join(ticket.key for ticket in res)
